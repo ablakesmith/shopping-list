f57727ed568e6db10d335d51b4b72e03943cf336
ablakesmith <a.blake.smith11@gmail.com>
2024-05-08 21:24:20 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import './App.css'; 
-import React, {useState, useEffect} from 'react'; 
 
-import ShoppingForm from './Components/ShoppingForm/ShoppingForm'; 
-import ShoppingList from './Components/ShoppingList/ShoppingList'; 
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import ShoppingForm from "./Components/ShoppingForm/ShoppingForm";
+import ShoppingList from "./Components/ShoppingList/ShoppingList";
 
-function App() { 
-  const [shoppingList, setShoppingList] = useState([]); 
-  useEffect(() => { 
-    fetch('https://9zdppw-8080.csb.app/api/list') 
-      .then(x => x.json()) 
-      .then(response => { 
-        setShoppingList(response); 
-      }); 
-  }, []); 
+function App() {
+    const [shoppingList, setShoppingList] = useState([]);
 
-  return ( 
-    <div className="App"> 
-      <header className="App-header"> 
-        <h1>Shopping List</h1> 
-      </header> 
-      <main> 
-        <ShoppingForm /> 
-        <ShoppingList items={shoppingList}/> 
-      </main> 
-    </div> 
-  ); 
-} 
+    function loadData() {
+        fetch("https://qr3zd2-8080.csb.app/api/list")
+            .then((x) => x.json())
+            .then((response) => {
+                setShoppingList(response);
+            });
+    }
 
-export default App; 
+    useEffect(loadData, []);
+
+    function addItem(item, quantity) {
+        fetch("https://qr3zd2-8080.csb.app/api/list/new", {
+            method: "POST",
+            body: JSON.stringify({ item, quantity }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+            mode: "cors",
+        })
+            .then((x) => x.json())
+            .then(loadData);
+    }
+
+    function deleteItem(id) {
+        fetch("https://qr3zd2-8080.csb.app/api/list/" + id, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+            mode: "cors",
+        })
+            .then((x) => x.json())
+            .then(loadData);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Shopping List</h1>
+            </header>
+            <main>
+                <ShoppingForm addItem={addItem} />
+                <ShoppingList items={shoppingList} deleteItem={deleteItem} />
+            </main>
+        </div>
+    );
+}
+
+export default App;
--- src/Components/ShoppingList/ShoppingList.jsx
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
-function ShoppingItem(props) {
-    return <li>{props.item} ({props.quantity})</li>
+function ShoppingItem({ id, item, quantity, deleteItem}) {
+
+    function handleDelete(event) {
+        event.preventDefault();
+        deleteItem(id);
+    }
+
+    return (
+        <li>
+            <span>{item} ({quantity})</span> 
+            <button onClick={handleDelete}>Delete</button>
+        </li>
+    );
 }
 
-export default function ShoppingList( { items }) {
-  
+
+export default function ShoppingList({ items, deleteItem }) {
+
     const ItemsJsx = items.map(listItem => 
-    <ShoppingItem 
-        key={listItem.id}
-        id={listItem.id}
-        item={listItem.item}
-        quantity={listItem.quantity}
-        />);
+        <ShoppingItem 
+            key={listItem.id} 
+            id={listItem.id}
+            item={listItem.item}
+            quantity={listItem.quantity}
+            deleteItem={deleteItem}
+        />
+    );
 
-
-
-    return <ul>{ ItemsJsx }</ul>
+    return <ul>{ ItemsJsx }</ul>;
 }
